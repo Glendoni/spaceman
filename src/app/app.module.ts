6c58cb71752db851569fc42687e30f473fddc516
Glendon Small <gsmall@sonovate.com>
2017-10-19 14:53:03 +0100

    Instant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule, Http } from '@angular/http';
 import { TranslateService, TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+
 export function createTranslateLoader(http: Http) {
     return new TranslateHttpLoader(http, './assets/static/i18n/', '.json');
 }
@@ -17,6 +19,7 @@ export function createTranslateLoader(http: Http) {
     HttpModule,
     BrowserAnimationsModule,
     BrowserModule,
+    CoreModule,
               TranslateModule.forRoot({
             loader: {
                 provide: TranslateLoader,
